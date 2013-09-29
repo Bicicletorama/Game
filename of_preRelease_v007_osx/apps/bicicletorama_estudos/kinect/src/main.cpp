f92d124562d9b64305b3c8207a81147ec9076d4c
Carlos de Oliveira Junior <vamoss@vamoss.com.br>
2013-09-28 23:25:52 -0300

    Remove v1

--- of_preRelease_v007_osx/apps/bicicletorama_estudos/kinect/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "kinectBox2d.h"
-#include "ofAppGlutWindow.h"
-
-int main() {
-	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024, 768, OF_FULLSCREEN);
-	ofRunApp(new kinectBox2d());
-}
--- of_preRelease_v007_osx/libs/openFrameworks/3d/of3dUtils.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void ofDrawAxis(float size);
